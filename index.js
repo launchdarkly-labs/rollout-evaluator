808b307532b301ea624415a8901272281caa47ce
Michal Fasanek <mfasanek@launchdarkly.com>
2022-10-05 14:33:48 +0200

    add parity calc & time to execute calc

--- index.js
+++ index.js
@@ -25,28 +25,46 @@ ldClient.waitForInitialization().then(function () {
   showMessage("SDK successfully initialized!");
 
   // Reading the content of a CSV file
-  const results = [];
+  const users = [];
   fs.createReadStream('assets/user-emails.csv')
     .pipe(csv())
-    .on('data', (data) => results.push(data))
+    .on('data', (data) => users.push(data))
     .on('end', () => {
       // Using the parsed CSV to generate user keys and evaluate them for a feature rollout
       console.log("STARTED evaluating the users");
+      const start = performance.now();
       let trueVar = 0;
       let falseVar = 0;
 
-      // slicedResults = results.slice(0,50);
+      slicedUsers = users.slice(0,20);
 
       async function bucketUsers () {
-        await Promise.all(results.map(async (result) => {
-          const user = {
-            "key": result.email,
+        await Promise.all(users.map(async (user) => {
+          
+          // Split the user email by "-" or "@" characters and take the second item in the resulting array (should be a number)
+          // User email format: test-[N]@email.com
+          const userNumber = parseInt(user.email.split(/[@,-]+/)[1]);
+          const determineParity = (number) => {
+            let parity
+            if (number % 2 == 0) {
+              parity = "even";
+            } else {
+              parity = "odd";
+            }
+            return parity
+          }
+          
+          const userContext = {
+            "key": user.email,
             "custom": {
-              "pre-bucket": true
+              "pre-bucket": true,
+              "parity": determineParity(userNumber)
             }
           };
 
-          ldClient.variation(featureFlagKey, user, false).then((flagValue) => {
+
+
+          ldClient.variation(featureFlagKey, userContext, false).then((flagValue) => {
             if (flagValue === true) {
               trueVar++;
             } else if (flagValue === false) {
@@ -59,7 +77,8 @@ ldClient.waitForInitialization().then(function () {
       }
 
       bucketUsers().then(() => {
-        console.log("USING PROMISE.ALL and THEN");
+        const end = performance.now();
+        console.log(`FINISHED evaluating users. Time to execute: ${(end - start).toFixed(2)} ms`);
         console.log(`Served TRUE variation: ${trueVar} user keys`);
         console.log(`Served FALSE variation: ${falseVar} user keys`);
 
