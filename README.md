13dd4a5757328e22c4fcb44e4a67ec8db126d181
Michal Fasanek <mfasanek@launchdarkly.com>
2022-10-13 21:31:29 +0200

    add mvp csv export capability

--- README.md
+++ README.md
@@ -2,5 +2,15 @@
 
 Simple Node application aiming to give LaunchDarkly users the ability to identify who'll be part of a percentage rollout. 
 
-It accepts a CSV file with a single column called `email`. The output is TBD but something that shows the user who's going to be part of a particular % rollout. Can be another CSV with just the email addresses that receive `true` flag value or perhaps a CSV with the complete user set, just with added column.
+It accepts a CSV file with a single column called `key`. The output is a CSV with two columns: one with user keys and the second with header matching the provided flag key and values matching the assigned variations for the particular % rollout.
+
+**tldr instructions for use:**
+
+* Clone the repository, enter the folder, and run `npm install`.
+* Create `.env` file in the root directory of your project and add a single variable: `SDK_KEY`. Set the SDK key of the LD environment where you want to do your rollout as its value.
+* Place a csv file with a single column holding your user keys inside the `source` folder. The header of the single column of data needs to be set to `key`.
+* Edit line 20 in the `index.js` file so it matches the flag key of the flag you want to roll out.
+* Edit line 30 in the `index.js` file so it points to your file.
+* Execute the script with `node index.js`.
+* If all goes well, you should see a new CSV file added to the `output` folder. Here you should see the variation assignment for the selected flag next to each user key.
 
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const LaunchDarkly = require('launchdarkly-node-server-sdk');
 const csv = require('csv-parser')
 const fs = require('fs')
+const { parseAsync } = require('json2csv');
 require('dotenv').config();
 
 function showMessage(s) {
@@ -26,24 +27,24 @@ ldClient.waitForInitialization().then(function () {
 
   // Reading the content of a CSV file
   const users = [];
-  fs.createReadStream('assets/user-emails.csv')
+  fs.createReadStream('source/user-emails.csv')
     .pipe(csv())
     .on('data', (data) => users.push(data))
     .on('end', () => {
-      // Using the parsed CSV to generate user keys and evaluate them for a feature rollout
+      // Using the parsed CSV to generate user keys and evaluate them for a feature rollout 
       console.log("STARTED evaluating the users");
       const start = performance.now();
       let trueVar = 0;
       let falseVar = 0;
 
       slicedUsers = users.slice(0,20);
-
+      console.log(slicedUsers);
       async function bucketUsers () {
-        await Promise.all(users.map(async (user) => {
+        await Promise.all(slicedUsers.map(async (user) => {
           
           // Split the user email by "-" or "@" characters and take the second item in the resulting array (should be a number)
           // User email format: test-[N]@email.com
-          const userNumber = parseInt(user.email.split(/[@,-]+/)[1]);
+          const userNumber = parseInt(user.key.split(/[@,-]+/)[1]);
           const determineParity = (number) => {
             let parity
             if (number % 2 == 0) {
@@ -55,14 +56,12 @@ ldClient.waitForInitialization().then(function () {
           }
           
           const userContext = {
-            "key": user.email,
+            "key": user.key,
             "custom": {
               "pre-bucket": true,
               "parity": determineParity(userNumber)
             }
           };
-
-
 
           ldClient.variation(featureFlagKey, userContext, false).then((flagValue) => {
             if (flagValue === true) {
@@ -72,12 +71,17 @@ ldClient.waitForInitialization().then(function () {
             } else {
               console.log(err);
             }
+            user[featureFlagKey] = flagValue;
+
+            return user;
           }).catch((err) => console.log(err));
         }))
       }
 
       bucketUsers().then(() => {
         const end = performance.now();
+
+        // console.log(slicedUsers);
         console.log(`FINISHED evaluating users. Time to execute: ${(end - start).toFixed(2)} ms`);
         console.log(`Served TRUE variation: ${trueVar} user keys`);
         console.log(`Served FALSE variation: ${falseVar} user keys`);
@@ -85,15 +89,23 @@ ldClient.waitForInitialization().then(function () {
         ldClient.flush(function () {
           ldClient.close();
         });
+
+        // Parse final JSON to a CSV
+        const fields = ['key', featureFlagKey];
+        const opts = { fields };
+
+        parseAsync(slicedUsers, opts)
+          .then(csv => fs.writeFileSync("output/export.csv", csv))
+          .catch(err => console.error(err));
       });
 
-      // TODO
-      // 1) Add logic to count the number of users for each flag variation -> DONE
-      // 2) Export this into a new CSV 
-      /* 3) Create UI: 
-               - Able to upload CSV
-               - Able to add SDK key
-               - Download output
+      /* TODO
+      *  1) Handle import/export logic
+      *  2) [Nice to have] Option to evaluate multiple flags
+      *  2) [Very nice to have] Create UI: 
+      *     - Able to upload CSV
+      *     - Able to add SDK key
+      *     - Download output
       */
     });
 
